5349ab153c9a67e5eb660b57f5541d71cd2fbe2f
tomwey <tomwey@163.com>
2019-06-19 12:49:31 +0800

    c

--- src/pages/stat-home/stat-home.ts
+++ src/pages/stat-home/stat-home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
 import { HomePage } from '../home/home';
 
+import ECharts from 'echarts';
+
 /**
  * Generated class for the StatHomePage page.
  *
@@ -31,7 +33,43 @@ export class StatHomePage {
 
   ionViewDidLoad() {
     // console.log('ionViewDidLoad StatHomePage');
+    var myChart = ECharts.init(document.getElementById('top-graphic') as HTMLDivElement);
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+        text: ''
+      },
+      series: [{
+        name: '',
+        type: 'pie',
+        radius: ['60%', '62%'],
+        silent: true,
+        data: [
+          { value: 132, name: '已完成计划' },
+          { value: 290, name: '未完成计划' }
+        ],
+        itemStyle: {
+          normal: {
+            label: {
+              show: false
+            },
+            labelLine: {
+              show: false
+            }
+          },
+
+        },
+      }],
+      color: ['rgb(231,90,22)', 'rgb(159,159,159)'],
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
   }
+
+  // ionViewDidEnter() {
+
+  // }
 
   segmentChanged(ev) {
 
