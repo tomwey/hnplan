8333c3798280177128cae84c779f457b902be094
tomwey <tomwey@163.com>
2019-06-20 09:13:26 +0800

    c

--- src/pages/stat-project/stat-project.module.ts
+++ src/pages/stat-project/stat-project.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { StatProjectPage } from './stat-project';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { StatProjectPage } from './stat-project';
   ],
   imports: [
     IonicPageModule.forChild(StatProjectPage),
+    ComponentsModule
   ],
 })
-export class StatProjectPageModule {}
+export class StatProjectPageModule { }
--- src/pages/stat-project/stat-project.ts
+++ src/pages/stat-project/stat-project.ts
@@ -15,11 +15,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class StatProjectPage {
 
+  dataType: number = 0;
+  dataTypes: any = [
+    '我的计划统计',
+    '部门计划统计'
+  ];
+
+  filterItems: any = [
+    {
+      name: '本月',
+      closable: false
+    },
+    {
+      name: '总裁交办',
+      closable: true
+    },
+    {
+      name: '枫丹西悦二期',
+      closable: true
+    },
+    {
+      name: '3级计划',
+      closable: true
+    },
+  ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad StatProjectPage');
+    // console.log('ionViewDidLoad StatProjectPage');
   }
+
+  segmentChanged(ev) {
+
+  }
+
+  openFilter() {
+
+  }
+
+  itemsChange() { }
 
 }
