90a337a62883541b376bcc82702a759f9f528c86
tomwey <tomwey@163.com>
2019-06-21 16:16:52 +0800

    c

--- src/pages/feedback-detail/feedback-detail.ts
+++ src/pages/feedback-detail/feedback-detail.ts
@@ -19,7 +19,65 @@ export class FeedbackDetailPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FeedbackDetailPage');
+    // console.log('ionViewDidLoad FeedbackDetailPage');
   }
+
+  data: any = [
+    {
+      label: '计划名称',
+      value: '计划管理系统APP端项目计划统计功能开发完成'
+    },
+    {
+      label: '计划类型',
+      value: '职能计划'
+    },
+    {
+      label: '计划完成时间',
+      value: '2019-07-09'
+    },
+    {
+      label: '反馈时间',
+      value: '2019-07-09'
+    },
+    {
+      label: '完成进度',
+      value: '50%'
+    },
+    {
+      label: '相关计划',
+      value: '系统APP端项目计划统计功能开发完成'
+    },
+    {
+      label: '风险等级',
+      value: '高风险'
+    },
+    {
+      label: '反馈说明',
+      value: '这是反馈说明，这是反馈说明，这是反馈说明，这是反馈说明，这是反馈说明，这是反馈说明，'
+    },
+    {
+      label: '反馈附件',
+      value: ''
+    }
+  ];
+
+  data2: any = [
+    {
+      name: '关于珍宝琥珀三期绿化太少的问题，要让专人去抓一下',
+      mannames: '张超,王超,李超'
+    },
+    {
+      name: '关于珍宝琥珀三期绿化太少的问题，要让专人去抓一下',
+      mannames: '张超,王超,李超'
+    },
+    {
+      name: '关于珍宝琥珀三期绿化太少的问题，要让专人去抓一下',
+      mannames: '张超,王超,李超'
+    },
+    {
+      name: '关于珍宝琥珀三期绿化太少的问题，要让专人去抓一下',
+      mannames: '张超,王超,李超'
+    },
+  ];
 
 }
